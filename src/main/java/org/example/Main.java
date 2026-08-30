03e1f2613e4e1342e2f006f25d7e252c67157be0
Vigo Matuka <vigo.matuka@gmail.com>
2026-08-30 21:23:33 +0200

    Napravljen AST parser koji iz List<Path> vraća List<ParsedMethod> sa svim metodama i njihovim atributima

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import org.example._1input.InputManager;
+import org.example._2parser.ASTParser;
+import org.example.model.ParsedMethod;
 
 import java.nio.file.Path;
 import java.util.List;
@@ -15,8 +17,24 @@ public class Main {
         InputManager inputManager = new InputManager();
         List<Path> files = inputManager.scanForFiles(path);
 
+        ASTParser astParser = new ASTParser();
+        List<ParsedMethod> parsedMethods = astParser.parseFiled(files);
+
         for (Path a : files){
             System.out.println(a);
         }
+
+        for (ParsedMethod a: parsedMethods){
+            System.out.println(a.getMethodName());
+            System.out.println(a.getClassName());
+            System.out.println(a.getFileName());
+            System.out.println(a.getLineNumber());
+            System.out.println(a.getCalledMethods());
+            System.out.println(a.getAnnotations());
+            System.out.println();
+        }
+
+
+
     }
 }
--- src/main/java/org/example/_2parser/ASTParser.java
+++ src/main/java/org/example/_2parser/ASTParser.java
@@ -1,4 +1,47 @@
 package org.example._2parser;
 
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.expr.AnnotationExpr;
+import com.github.javaparser.ast.expr.MethodCallExpr;
+import org.example.model.ParsedMethod;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 public class ASTParser {
+
+    public List<ParsedMethod> parseFiled(List<Path> files) throws IOException {
+        List<ParsedMethod> parsiraneMetode = new ArrayList<>();
+
+        for (Path file : files){
+            CompilationUnit cu = StaticJavaParser.parse(file);
+            List<MethodDeclaration> metode = cu.findAll(MethodDeclaration.class);
+            for(MethodDeclaration metoda : metode){
+                String name = metoda.getNameAsString();
+                String className = metoda
+                        .findAncestor(ClassOrInterfaceDeclaration.class)
+                        .map(ClassOrInterfaceDeclaration::getNameAsString)
+                        .orElse("UNKNOWN");
+                String filenName = file.getFileName().toString();
+                int lineNumber = metoda.getBegin().map(p -> p.line).orElse(-1); //-1 je sentinel vrijednost
+                List<String> calledMethods = metoda.findAll(MethodCallExpr.class).stream()
+                        .map(MethodCallExpr::getNameAsString)
+                        .collect(Collectors.toList()); //collect sakuplja rezultat u strukturu, a Colelctor je pomocna klasa
+                List<String> annotations = metoda.getAnnotations().stream()
+                        .map(AnnotationExpr::getNameAsString)
+                        .collect(Collectors.toList());
+                parsiraneMetode.add(new ParsedMethod(name, className, filenName, lineNumber, calledMethods, annotations));
+            }
+        }
+
+        return parsiraneMetode;
+    }
+
 }
