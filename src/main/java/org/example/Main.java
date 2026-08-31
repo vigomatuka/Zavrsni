218cd2e1aeb51b568e57f7b41b374b70d850a63a
Vigo Matuka <vigo.matuka@gmail.com>
2026-08-31 15:19:06 +0200

    Podijeljeno parsiranje na prema sustavima/mikroservisim za lakšu izgradnju grafa i analizu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,19 +12,37 @@ import java.util.List;
 public class Main {
     static void main() throws Exception{
 
-        Path path = Path.of("C:\\Users\\Vigo\\Downloads\\test-input-primjer");
+        Path repo1 = Path.of("C:\\Users\\Vigo\\Downloads\\test-input-primjer\\servis-a-users");
+        Path repo2 = Path.of("C:\\Users\\Vigo\\Downloads\\test-input-primjer\\servis-b-orders");
 
         InputManager inputManager = new InputManager();
-        List<Path> files = inputManager.scanForFiles(path);
+        List<Path> files1 = inputManager.scanForFiles(repo1);
+        List<Path> files2 = inputManager.scanForFiles(repo2);
 
         ASTParser astParser = new ASTParser();
-        List<ParsedMethod> parsedMethods = astParser.parseFiled(files);
+        List<ParsedMethod> parsedMethods1 = astParser.parseFiled(files1);
+        List<ParsedMethod> parsedMethods2 = astParser.parseFiled(files2);
 
-        for (Path a : files){
+
+        for (Path a : files1){
             System.out.println(a);
         }
 
-        for (ParsedMethod a: parsedMethods){
+        for (Path a : files2){
+            System.out.println(a);
+        }
+
+        for (ParsedMethod a: parsedMethods1){
+            System.out.println(a.getMethodName());
+            System.out.println(a.getClassName());
+            System.out.println(a.getFileName());
+            System.out.println(a.getLineNumber());
+            System.out.println(a.getCalledMethods());
+            System.out.println(a.getAnnotations());
+            System.out.println();
+        }
+
+        for (ParsedMethod a: parsedMethods2){
             System.out.println(a.getMethodName());
             System.out.println(a.getClassName());
             System.out.println(a.getFileName());
