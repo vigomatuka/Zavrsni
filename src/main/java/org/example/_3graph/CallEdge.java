b3c5ede28dc9d52af04e00f09072142b876b0a22
Vigo Matuka <vigo.matuka@gmail.com>
2026-09-01 16:02:08 +0200

    Napravljena MethodNode klasa za custom node za graf, stvorene CallEdge i CallGraphBuilder klase

---
+++ src/main/java/org/example/_3graph/CallEdge.java
@@ -0,0 +1,4 @@
+package org.example._3graph;
+
+public class CallEdge {
+}
---
+++ src/main/java/org/example/_3graph/MethodNode.java
@@ -0,0 +1,38 @@
+package org.example._3graph;
+
+import java.util.Objects;
+
+public class MethodNode {
+    private String className;
+    private String methodName;
+    private boolean entryPoint = false;
+
+    public MethodNode(String className, String methodName){
+        this.className = className;
+        this.methodName = methodName;
+    }
+
+    public String getClassName(){
+        return className;
+    }
+    public String getMethodName(){
+        return methodName;
+    }
+    public boolean getEntryPoint(){
+        return entryPoint;
+    }
+    public void markAsEntryPoint(){
+        this.entryPoint = true;
+    }
+    @Override
+    public int hashCode(){
+        return Objects.hash(className, methodName);
+    }
+    @Override
+    public boolean equals(Object o){ //potreban za pozadinske provjere kod staranja cvorova i stavljanja u mapu
+        if (this == o) return true;
+        if (!(o instanceof MethodNode other)) return false; //sa svaki slucaj
+        // moze biti bez other sa: MethodNode other = (MethodNode) o;
+        return className.equals(other.className) && methodName.equals(other.methodName);
+    }
+}
